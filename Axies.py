0a18ffe4bea613f205441d4ca7d63c3d61e9e081
angel <angelanton01@gmail.com>
2021-08-07 17:56:04 -0400

    version 0.1.3.3

--- Axies.py
+++ Axies.py
@@ -1,7 +1,10 @@
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ChatAction
+import telebot
+import os
+
+tb=telebot.TeleBot(os.environ.get("TOKEN_AXIEFUTUREBOT"))
 
 button_info=InlineKeyboardButton(text="Más información", url="https://whitepaper.axieinfinity.com/")
-button_start=InlineKeyboardButton(text="Volver al menu principal", callback_data="start")
 
 def juego(update, context):
 
@@ -9,16 +12,28 @@ def juego(update, context):
     query.answer()
 
     query.edit_message_text(text="¿Has escuchado de Axie Infinty pero no te queda claro que es?\n\n"\
-        "¡No te procupes! Ahora lo entenderas todo\n\n"\
+        "¡No te procupes! Ahora lo entenderas todo. "\
         "Axie Infinity es un juego de mascotas donde los jugadores luchan, crian y comercian "\
         "con criaturas de fantasia llamadas Axies\n\n"\
         "El juego fue desarrollado por Sky Mavis y esta claramente inspirado en juegos como pokemon o tamagochi, "\
         "ademas de tener fuertes influencias de juegos de cartas\n\n"\
-        "Algunos socios del proyecto Axie Infinity son: Samsung, Ubisoft y Binance, etc...\n\n",
+        "Algunos socios del proyecto Axie Infinity son: Samsung, Ubisoft y Binance, etc...\n\n"\
+        "Gracias por usar AxieFutureBot\n"\
+        "Para volver a vernos escribe \"/start\"\n\n",
     reply_markup=InlineKeyboardMarkup([
-        [button_info],
-        [button_start]
+        [button_info]
     ]))
+
+    chat_id=update.effective_message.chat.id
+
+    chat=update.effective_message.chat
+    
+    chat.send_action(
+        action=ChatAction.UPLOAD_PHOTO,
+        timeout=None
+    )
+    
+    tb.send_sticker(chat_id,open("imagenes/webp/juego.webp", "rb"))
 
 def axie(update, context):
 
@@ -29,7 +44,18 @@ def axie(update, context):
         "¡Genial! veamos...\n\n"\
         "Los Axies son las adorables y feroces criaturas que protagonizan Axie Infinity. "\
         "Viven dentro del mundo de Lunacia y seran nuestras mascotas. "\
-        "Mascotas que usaremos para pelear contra otros jugadores y enemigos del juego en combates por turnos en tiempo real\n\n",
-    reply_markup=InlineKeyboardMarkup([
-        [button_start]
-    ]))
+        "Mascotas que usaremos para pelear contra otros jugadores y enemigos del juego en combates por turnos en tiempo real\n\n"\
+        "Gracias por usar AxieFutureBot\n"\
+        "Para volver a vernos escribe \"/start\"\n\n"
+    )
+
+    chat_id=update.effective_message.chat.id
+
+    chat=update.effective_message.chat
+    
+    chat.send_action(
+        action=ChatAction.UPLOAD_PHOTO,
+        timeout=None
+    )
+    
+    tb.send_sticker(chat_id,open("imagenes/webp/axie.webp", "rb"))
